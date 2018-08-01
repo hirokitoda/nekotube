c891b0034c052491638bafa05bb590058aaea3bb
hirokitoda <hirokimusic123@gmail.com>
2018-08-01 16:45:02 +0900

    note10

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './components/Header/Header';
 import YSearch from 'youtube-api-search';
 
-const YOUTUBE_API_KEY = 'AIzaSyAypMwkSgPExT_Sm1e6ckqlgG8GqysFxkY'
+import Header from './components/Header/Header';
+import Body from './components/Body/Body';
+import List from './components/Video/List/List';
+
+
+const YOUTUBE_API_KEY = 'AIzaSyC4Zaowu05ZczH-jEwAhIs3iU1JrgKRNk8'
 
 class App extends Component {
 
-state = { videos: [] }
+  state = { videos: [] }
 
-componentDidMount(){
-  YSearch({ key: YOUTUBE_API_KEY, term: '猫　きゅうり'},
-    (data) => {this.setState({videos: data});
-  });
-}
-
+  componentDidMount(){
+    YSearch({ key: YOUTUBE_API_KEY, term: '猫　きゅうり'}, (data) => {
+ 　    this.setState({ videos: data })
+　  });
+  }
 
   render() {
     return (
       <div className="App">
         <Header />
+        <Body><List videos={this.state.videos} /></Body>
       </div>
     );
   }
---
+++ src/components/Video/List/List.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Item from '../Item/Item';
+const List = (props) => {
+
+const Items = props.videos.map((video,index) => {
+  return <Item video={video} />;
+});
+
+  return (
+    <ul className="col-md-4 list-group">
+      { Items }
+    </ul>
+  );
+};
+
+export default List;
