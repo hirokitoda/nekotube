05aba7747fa24d7ff785fed7dca7335383b9a42f
hirokitoda <hirokimusic123@gmail.com>
2018-08-01 13:10:34 +0900

    note8

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import YSearch from 'youtube-api-search';
+
+const YOUTUBE_API_KEY = 'AIzaSyAypMwkSgPExT_Sm1e6ckqlgG8GqysFxkY'
+
+YSearch({ key: YOUTUBE_API_KEY, term: '猫　きゅうり'}, (data) => {
+  console.log(data);
+});
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
